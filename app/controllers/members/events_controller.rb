53773c7ec110f23f26c0ad98e340f6caa6620683
vagrant <vagrant@localhost.localdomain>
2020-11-09 10:56:12 +0000

    post表示順修正/views再修正

--- app/controllers/members/events_controller.rb
+++ app/controllers/members/events_controller.rb
@@ -3,7 +3,7 @@ class Members::EventsController < ApplicationController
   def index
     @season = Season.find(params[:season_id])
     @events = @season.events
-    @posts = Post.where(event_id: params[:season_id])
+    @posts = Post.where(event_id: params[:season_id]).page(params[:page]).reverse_order
   end
 
 end
